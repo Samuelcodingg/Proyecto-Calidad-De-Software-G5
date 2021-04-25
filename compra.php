aca7f6661a6c14be56182271b176093e8ec1ac59
Samuelcodingg <66806293+Samuelcodingg@users.noreply.github.com>
2021-04-25 11:59:12 -0500

    correciones pagina compra

--- compra.php
+++ compra.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="css/sweetalert2.min.css">
     <link rel="stylesheet" href="css/index1.css?v=<?php echo time(); ?>">
 
-    <title>Carrito Compras en JavaScript</title>
+    <title>HADTECSOFT</title>
 
 </head>
 
@@ -41,11 +41,25 @@
                 <img src="image/Logo HadtecSoft (1).png" width="125px">
             </div>
             <div class="nav-items">
-                <li><a href="index.php">Inicio</a></li>
-                <!-- <li><a href="#">Nosotros</a></li> -->
-                <li><a href="productos.php">Catálogo</a></li>
-                <!-- <li><a href="#">Contacto</a></li> -->
-                <li class="anchar"><a href="login.php">Sesión</a></li>
+            <?php
+                    session_start();
+                    if(!empty($_SESSION['username'])){
+                        $nombre = $_SESSION['username'];
+                        $apellido = $_SESSION['apellido'];
+                        $email = $_SESSION['email'];
+                        ?>
+                            <li><a href="index.php">Inicio</a></li>
+                            <!-- <li><a href="#">Nosotros</a></li> -->
+                            <li><a href="productos.php">Catálogo</a></li>
+                            <li><a href="soporte.php">Contacto</a></li>
+                            <li class="anchar"><a href="#"><?php echo $nombre;?></a></li>
+                            <li><a href="login.php">Salir</a></li>
+                        <?php
+                    }
+                    else{
+                        header("location:login.php");
+                    }
+                ?>
                 <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -110,13 +124,13 @@
                             <label for="cliente" class="col-12 col-md-2 col-form-label h2">Cliente :</label>
                             <div class="col-12 col-md-10">
                                 <input type="text" class="form-control" id="cliente"
-                                    placeholder="Ingresa nombre cliente" name="destinatario">
+                                    placeholder="<?php echo $nombre.' '.$apellido;?>" name="destinatario">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="email" class="col-12 col-md-2 col-form-label h2">Correo :</label>
                             <div class="col-12 col-md-10">
-                                <input type="text" class="form-control" id="correo" placeholder="Ingresa tu correo" name="cc_to">
+                                <input type="text" class="form-control" id="correo" placeholder="<?php echo $email;?>" name="cc_to">
                             </div>
                         </div>
 
@@ -170,7 +184,7 @@
                                 <a href="productos.php" class="btn btn-info btn-block">Seguir comprando</a>
                             </div>
                             <div class="col-xs-12 col-md-4">
-                                <input type="submit" class="btn btn-success btn-block" id="procesar-compra" value="Realizar compra">
+                                <input type="submit" class="btn btn-success btn-block" id="procesar-compra" value="Finalizar Compra">
                             </div>
                         </div>
                     </form>
